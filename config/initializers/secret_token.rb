52a9a6db25b7c3c627a8b45141ec54a2bd80178d
Transient Shadowsteel <transient@shadowsteel.net>
2014-01-31 19:53:04 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '79a1b1dd554d4c876667f379155b30259e8fd370b8c0acc14ebf3710d5185174a3f93724f34c33d3b31ce7eb334efb5e12a2a6dfbcbbfcc8cc469d6948888f9d'
